7eb5eb815b5e970bcd1b8b4124f1adb3eda04f26
Lane Bretschneider <lanebret85@gmail.com>
2023-07-19 08:49:05 -0700

    feat: add passenger class with initialize, speeding?, speed, add_passenger, and num_adults methods

---
+++ lib/vehicle.rb
@@ -0,0 +1,33 @@
+class Vehicle
+  attr_reader :year, :make, :model, :passengers
+
+  def initialize(year, make, model)
+    @year = year
+    @make = make
+    @model = model
+    @speeding = false
+    @passengers = []
+  end
+
+  def speeding?
+    @speeding
+  end
+
+  def speed
+    @speeding = true
+  end
+
+  def add_passenger(passenger)
+    @passengers << passenger
+  end
+
+  def num_adults
+    adult_passengers = []
+    @passengers.find_all {
+      |passenger| if passenger.adult?
+        adult_passengers << passenger
+      end
+    }
+    adult_passengers.count
+  end
+end
